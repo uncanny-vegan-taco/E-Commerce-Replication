fc2a51a871319c1d697a57d61b4eaaf60121e194
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-12-19 14:52:11 -0800

    Final Reset

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navigation />
+      </header>
+    </div>
+  );
+}
+
+export default App;
