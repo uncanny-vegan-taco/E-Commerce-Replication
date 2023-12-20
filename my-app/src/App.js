6daa8d58149f018c778a64117d5207ff035cafad
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-12-20 14:26:34 -0800

    Adding Slideshow

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./Navigation";
+import Slideshow from "./Slideshow";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navigation />
+        <Slideshow />
       </header>
     </div>
   );
---
+++ my-app/src/Slideshow.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Carouselimage1 from "./Carouselimage1.png";
+import Carouselimage2 from "./Carouselimage2.png";
+import Carouselimage3 from "./Carouselimage3.png";
+export default Slideshow;
+
+function Slideshow() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <Carouselimage1 text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Carouselimage2 text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Carouselimage3 text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
