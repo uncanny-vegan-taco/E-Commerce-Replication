09094b97ff7673b5ded09a26d2f540631c660e47
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-12-20 14:33:43 -0800

    Slideshow working

--- my-app/src/Slideshow.js
+++ my-app/src/Slideshow.js
@@ -16,21 +16,21 @@ function Slideshow() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <Carouselimage1 text="First slide" />
+        <img src={Carouselimage1} />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Carouselimage2 text="Second slide" />
+        <img src={Carouselimage2} />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Carouselimage3 text="Third slide" />
+        <img src={Carouselimage3} />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
