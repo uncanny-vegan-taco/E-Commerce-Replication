ffaae07cfd3703d2c8801c7de7cd4de2d2c1dbdd
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-12-20 15:12:02 -0800

    Panel beneath carousel

--- my-app/src/Slideshow.js
+++ my-app/src/Slideshow.js
@@ -4,6 +4,8 @@ import Carousel from "react-bootstrap/Carousel";
 import Carouselimage1 from "./Carouselimage1.png";
 import Carouselimage2 from "./Carouselimage2.png";
 import Carouselimage3 from "./Carouselimage3.png";
+import "./Slideshow.css";
+import Container from "react-bootstrap/esm/Container";
 export default Slideshow;
 
 function Slideshow() {
@@ -14,30 +16,42 @@ function Slideshow() {
   };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img src={Carouselimage1} />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src={Carouselimage2} />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src={Carouselimage3} />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <Container id="Container">
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <a href="#">
+            <img src={Carouselimage1} className="Slide" />
+          </a>
+        </Carousel.Item>
+        <Carousel.Item>
+          <a href="#">
+            <img src={Carouselimage2} className="Slide" />
+          </a>
+        </Carousel.Item>
+        <Carousel.Item>
+          <a href="#">
+            <img src={Carouselimage3} className="Slide" />
+          </a>
+        </Carousel.Item>
+      </Carousel>
+      <div className="Footer">
+        <div className="footer-block">
+          <p>Hi</p>
+          <a href="#">Sign In</a>
+        </div>
+        <div className="footer-block">
+          <p>Save 35% Today</p>
+          <a href="#">Set up Autoship</a>
+        </div>
+        <div className="footer-block">
+          <p>Recent Order</p>
+          <a href="#">Track Package</a>
+        </div>
+        <div className="footer-block">
+          <p>Chewy Pharmacy</p>
+          <a href="#">Shop Now</a>
+        </div>
+      </div>
+    </Container>
   );
 }
